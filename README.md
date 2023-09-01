ba5a01a9c2fbb023be667acb7f65c4d74cc9cea3
quangchinh186 <quangchinh1122@gmail.com>
2023-09-01 13:47:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mobile App Dev
+ INT3120_20
---
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle
+
+public class TestActivity extends AppCompatActivity {
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+    }
+}
