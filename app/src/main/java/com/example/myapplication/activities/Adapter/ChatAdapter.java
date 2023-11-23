6a208f804fb4ef7923d03c5484e291f822935c86
Tien Dung <dung14b@gmail.com>
2023-11-24 03:10:03 +0700

    chat feature
    complete

---
+++ app/src/main/java/com/example/myapplication/activities/Adapter/ChatAdapter.java
@@ -0,0 +1,91 @@
+package com.example.myapplication.activities.Adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.activities.ApplicationActivity;
+import com.example.myapplication.schema.ChatMessage;
+import com.example.myapplication.system.BatoSystem;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatModelViewHolder> {
+  Context context;
+  List<ChatMessage> messages;
+
+  public ChatAdapter(Context context, List<ChatMessage> messages) {
+    this.context = context;
+    this.messages = messages;
+  }
+
+  public ChatModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(context).inflate(R.layout.chat_message_recycler,parent,false);
+    return new ChatModelViewHolder(view);
+  }
+
+  private String standardizeTime(String time){
+    String[] timeSplit = time.split(" ");
+    StringBuilder standardTime = new StringBuilder();
+    for (int i = 0; i < 4; i++) {
+      standardTime.append(timeSplit[i]).append(" ");
+    }
+    return standardTime.toString();
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull ChatModelViewHolder holder, int position) {
+    Log.d("ChatAdapter", "onBindViewHolder: "+messages.size());
+
+      //Log.d("ChatAdapter", "onBindViewHolder: "+message.getMessage());
+      if(messages.get(position).getFrom().equals(ApplicationActivity.user.getId())){
+        holder.leftChatLayout.setVisibility(View.GONE);
+        holder.rightChatLayout.setVisibility(View.VISIBLE);
+        holder.rightChatTextview.setText(messages.get(position).getMessage());
+        holder.sendTime.setText(standardizeTime(messages.get(position).getDateTime().toString()));
+      } else {
+        holder.rightChatLayout.setVisibility(View.GONE);
+        holder.leftChatLayout.setVisibility(View.VISIBLE);
+        holder.leftChatTextview.setText(messages.get(position).getMessage());
+        holder.receiveTime.setText(standardizeTime(messages.get(position).getDateTime().toString()));
+        Picasso.get()
+                .load("https://i.imgur.com/w4mkpDj.jpg")
+                .into(holder.senderChatImage);
+      }
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return messages.size();
+  }
+
+  class ChatModelViewHolder extends RecyclerView.ViewHolder{
+
+    LinearLayout leftChatLayout,rightChatLayout;
+    TextView leftChatTextview,rightChatTextview;
+    ImageView senderChatImage;
+    TextView sendTime, receiveTime;
+
+    public ChatModelViewHolder(@NonNull View itemView) {
+      super(itemView);
+      leftChatLayout = itemView.findViewById(R.id.received_message_container);
+      rightChatLayout = itemView.findViewById(R.id.sent_message_container);
+      leftChatTextview = itemView.findViewById(R.id.received_message_textview);
+      rightChatTextview = itemView.findViewById(R.id.sent_message_textview);
+      senderChatImage = itemView.findViewById(R.id.chat_sender_image);
+      sendTime = itemView.findViewById(R.id.sent_message_time_textview);
+      receiveTime = itemView.findViewById(R.id.received_message_time_textview);
+    }
+  }
+}
--- app/src/main/java/com/example/myapplication/activities/Conversation.java
+++ app/src/main/java/com/example/myapplication/activities/Conversation.java
@@ -1,18 +1,27 @@
 package com.example.myapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.activities.Adapter.ChatAdapter;
 import com.example.myapplication.schema.ChatMessage;
+import com.squareup.picasso.Picasso;
 
 import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
 
 import io.realm.OrderedRealmCollectionChangeListener;
 import io.realm.RealmQuery;
@@ -21,47 +30,74 @@ import io.realm.RealmResults;
 public class Conversation extends AppCompatActivity {
     private ObjectId roomId;
     private RealmQuery<ChatMessage> realmQuery;
+
+    ObjectId oppo;
     TextView title;
     EditText messageInput;
     TextView messages;
+    ChatAdapter chatAdapter;
+    RecyclerView chatRecycler;
+
+    ArrayList<ChatMessage> historyMessages = new ArrayList<>();
+
+    void setUpChatAdapter(){
+        chatAdapter = new ChatAdapter(this, historyMessages);
+        chatRecycler.setLayoutManager(new LinearLayoutManager(this));
+        chatRecycler.setAdapter(chatAdapter);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
         roomId = new ObjectId(intent.getStringExtra("chatId"));
+        oppo = ApplicationActivity.queryHelper.getFriend(roomId);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
+        messageInput = findViewById(R.id.message_edit_text);
         //UI
-        title = findViewById(R.id.chatName);
-        title.setText(roomId.toString());
-        messageInput = findViewById(R.id.message);
-        messages = findViewById(R.id.messages);
+        chatRecycler = findViewById(R.id.chat_recycler_view);
         //business logic
         realmQuery = ApplicationActivity.queryHelper.getRealmQuery(roomId);
         addChangeListener();
         getMessages();
+        setUpChatAdapter();
+        ImageView avtSender = findViewById(R.id.sender_image);
+        Picasso.get()
+                .load("https://i.imgur.com/w4mkpDj.jpg")
+                .into(avtSender);
+        chatRecycler.scrollToPosition(historyMessages.size() - 1);
     }
 
-    public void send(View view){
+    public void onBackButtonClicked(View view){
+        onBackPressed();
+    }
+
+    public void onSendMessage(View view){
         String m = messageInput.getText().toString();
         if(m.equals("")){
             return;
         }
         messageInput.setText("");
         ApplicationActivity.queryHelper.sendMessage(roomId, ApplicationActivity.user.getId(), m);
+        addChangeListener();
+        chatRecycler.scrollToPosition(historyMessages.size() - 1);
     }
 
     public void getMessages(){
-        String m = realmQuery.findAll().asJSON();
-        messages.setText(m);
+        RealmResults<ChatMessage> results = realmQuery.findAll();
+        historyMessages.addAll(results);
     }
 
     public void addChangeListener(){
         OrderedRealmCollectionChangeListener<RealmResults<ChatMessage>> changeListener = (collection, changeSet) -> {
             if(changeSet.getInsertions().length != 0){
-                String m = realmQuery.findAll().asJSON();
-                messages.setText(m);
+                Log.v("checking_realm", "inserted");
+                getMessages();
+                setUpChatAdapter();
+                chatRecycler.setAdapter(chatAdapter);
             }
         };
+        Log.v("checking_realm", "addChangeListener");
         realmQuery.findAll().addChangeListener(changeListener);
     }
 
